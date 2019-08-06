a8c6b7c15d576f01cf199148965abd4d712983af
Dave Ritchie <ritchiedw@hotmail.com>
2019-08-06 18:12:27 +0100

    getting actual weather data from openweathermap api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'd4cceb508acef381e49eb5a047434397';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,46 +10,31 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
 
-  void getData() async {
-    http.Response response = await http.get('https://samples.openweathermap'
-        '.org/data/2'
-        '.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-      //print(data);
-      //double longitude = jsonDecode(data)['coord']['lon'];
-      //print(longitude);
-      //int weatherDescription = jsonDecode(data)['weather'][0]['description'];
-      //print(weatherDescription);
-      //var decodedData = jsonDecode(data);
-      double temperature = jsonDecode(data)['main']['temp'];
-      int condition = jsonDecode(data)['weather'][0]['id'];
-      String cityName = jsonDecode(data)['name'];
-    } else {
-      print(response.statusCode);
-    }
+    NetworkHelper networkHelper =
+        NetworkHelper('https://api.openweathermap.org/data/2'
+            '.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-//    print(response.body);
-//    print(response.statusCode);
+    var weatherData = await networkHelper.getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
