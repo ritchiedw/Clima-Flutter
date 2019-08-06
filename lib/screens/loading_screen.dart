5889a0668745b714a383c525d4eb118b998985d0
Dave Ritchie <ritchiedw@hotmail.com>
2019-08-06 18:59:26 +0100

    passing data to a state object

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,9 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
@@ -25,19 +22,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentLocation();
 
-    latitude = location.latitude;
-    longitude = location.longitude;
-
-    NetworkHelper networkHelper =
-        NetworkHelper('https://api.openweathermap.org/data/2'
-            '.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2'
+        '.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey'
+        '&units'
+        '=metric');
 
     var weatherData = await networkHelper.getData();
 
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) {
-        return LocationScreen();
+        return LocationScreen(
+          locationWeather: weatherData,
+        );
       }),
     );
   }
