6a3ac3ec8637c488ea62fe7fdba1c6249694cf3e
Dave Ritchie <ritchiedw@hotmail.com>
2019-08-08 19:58:01 +0100

    Creating and styling a textfield

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,10 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: kTextFieldInputDecoration,
+                ),
               ),
               FlatButton(
                 onPressed: () {},
